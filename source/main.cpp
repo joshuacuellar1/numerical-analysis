88b4340d6003813c6ac03fe973601da79de7ddd4
Joshua Cuellar <joshuac.cuellar@outlook.com>
2025-05-21 23:39:57 +0000

    Add CLI11 library
    Signed-off-by: Joshua Cuellar <joshuac.cuellar@outlook.com>

--- source/main.cpp
+++ source/main.cpp
@@ -1,10 +1,34 @@
-//
-// Created by root on 5/3/25.
-//
+/*
+Copyright (c) 2025, Joshua Cuellar.
+*/
 
+
+#include <exception>
+#include <string>
+
+#include <CLI/CLI.hpp>
 #include <fmt/base.h>
 
-int main() {
-    fmt::print("Hello World!\n");
-    return 0;
+
+auto main(int argc, const char *argv[]) -> int {
+    try {
+        auto app = CLI::App("Numerical Analysis", "numerical_analysis");
+        std::string message;
+        app.add_option("-m,--message", message, "Print a message")
+                ->default_val("Hello World!");
+        try {
+            (app).parse(argc, argv);
+        } catch (const CLI::ParseError &e) {
+            return (app).exit(e);
+        }
+        // print_message(message);
+        fmt::print("{}\n", message);
+        return 0;
+    } catch (const std::exception &e) {
+        fmt::print(stderr, "Unhandled exception: {}\n", e.what());
+        return 1;
+    } catch (...) {
+        fmt::print(stderr, "Unknown exception\n");
+        return 2;
+    }
 }
